b8157c765dce6ebbf3f5494e06fe08b4ec3baf7f
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-09 15:31:29 -0500

    import input data

--- src/main/java/importDeck/ImportDeckInputData.java
+++ src/main/java/importDeck/ImportDeckInputData.java
@@ -1,4 +1,19 @@
 package importDeck;
 
 public class ImportDeckInputData {
+    private String fileName;
+    private String fileContents;
+
+    public ImportDeckInputData(String fileName, String fileContents){
+        this.fileName = fileName;
+        this.fileContents = fileContents;
+    }
+
+    String getFileName(){
+        return fileName;
+    }
+
+    String getFileContents(){
+        return fileContents;
+    }
 }
