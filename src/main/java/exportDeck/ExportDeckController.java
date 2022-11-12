751a288861c47db1223fcb265db9240c56b2b185
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-12 16:01:14 -0500

    started work on deck export system

--- src/main/java/exportDeck/ExportDeckController.java
+++ src/main/java/exportDeck/ExportDeckController.java
@@ -1,4 +1,20 @@
+/*
+ * Author: Aldo Bani
+ * Date: November 9/22
+ * This is the controller for the deck export system
+ */
+
 package exportDeck;
 
 public class ExportDeckController {
+    private final ExportDeckInputBoundary exportDeckInputBoundary;
+
+
+    public ExportDeckController(ExportDeckInputBoundary exportDeckInputBoundary) {
+        this.exportDeckInputBoundary = exportDeckInputBoundary;
+    }
+
+    public void runExport(){
+
+    }
 }
