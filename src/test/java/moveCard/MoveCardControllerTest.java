75a878fb348adfa57b949b785b13a1a4d58c3eb9
derrickcho3715 <derrick.cho@mail.utoronto.ca>
2022-11-13 23:25:12 -0500

    MoveCardFeat created tests

---
+++ src/test/java/moveCard/MoveCardControllerTest.java
@@ -0,0 +1,12 @@
+package moveCard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveCardControllerTest {
+
+    @Test
+    void move() {
+    }
+}
