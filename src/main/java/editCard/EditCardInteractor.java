51ed39a5a201d7f87b0d134e2aecb5cb930f3e74
Albert <albertyan2003@hotmail.com>
2022-11-18 19:08:07 -0500

    Update to Edit Card Packages Changes to EditCardInputData such that it uses card id number rather than card itself, now accepts editing of options with overloaded constructors EditCardInputBoundary changes as required
    Updated implementations for:
    EditCardInteractor
    EditCardInputBoundary
    EditCardController

--- src/main/java/editCard/EditCardInteractor.java
+++ src/main/java/editCard/EditCardInteractor.java
@@ -1,18 +1,28 @@
 package editCard;/*
  * Author: Albert Yan
  * Date: October 31/22
- * The use case interactor for editing cards a
+ * The use case interactor for editing cards given an input boundary
  */
 
-public class EditCardInteractor {
+import createCard.CreateCardInputBoundary;
+import entities.Flashcard;
+import entities.MCFlashcard;
 
-    private EditCardOutputBoundary editCardOutputBoundary;
+public class EditCardInteractor implements EditCardInputBoundary {
+
+    final EditCardOutputBoundary editCardOutputBoundary;
 
     public EditCardInteractor(EditCardOutputBoundary editCardOutputBoundary) {
         this.editCardOutputBoundary = editCardOutputBoundary;
     }
 
-    public void edit() {
-
+    @Override
+    public void edit(EditCardInputData inputData) {
+        Flashcard card = Flashcard.getTracker().get(inputData.getCardID());
+        card.setQuestion(inputData.getQuestion());
+        card.setAnswer(inputData.getQuestion());
+        if (inputData.getOptions() != null) {
+            ((MCFlashcard) card).setOptions(inputData.getOptions());
+        }
     }
 }
