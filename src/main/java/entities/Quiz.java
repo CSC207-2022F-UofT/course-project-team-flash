55507f6b4387b1a56882790f0afa61e6be385bfe
tsoisze2 <jasmine.tsoimud@gmail.com>
2022-11-20 15:40:02 -0500

    Changed uniqueID to quizName in Quiz class

--- src/main/java/entities/Quiz.java
+++ src/main/java/entities/Quiz.java
@@ -1,13 +1,15 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Quiz {
 
-    private final String quizName;
-    private final List<Deck> decks;
+    private String quizName;
+    private List<Deck> decks;
+
     private static final Map<String, Quiz> quizTracker = new HashMap<>();
 
     /**
@@ -15,13 +17,23 @@ public class Quiz {
      * @param quizName a String representing the name of this quiz
      * @param decks a List of decks that is included in this quiz
      */
+
     public Quiz(String quizName, List<Deck> decks) {
         this.quizName = quizName;
         this.decks = decks;
     }
 
-    public static Map<String, Quiz> getTracker() {
-        return quizTracker;
+    public List<Deck> getDecks(){
+        return this.decks;
+    }
+
+    public String getQuizName(){
+        return this.quizName;
+    }
+
+    public static Quiz getQuiz(String quizName){
+        return quizTracker.get(quizName);
+
     }
     public static void addTracker(String quizName, Quiz quiz) {
         quizTracker.put(quizName, quiz);
@@ -29,4 +41,6 @@ public class Quiz {
     public static void removeTracker(String quizName) {
         quizTracker.remove(quizName);
     }
+
+
 }
