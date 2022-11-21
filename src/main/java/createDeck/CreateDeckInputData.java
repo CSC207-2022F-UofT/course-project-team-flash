df1fc2c659b0fe751518d2dcc9f1f8138ea9cc5a
Jay <jay.kanchankoti@mail.utoronto.ca>
2022-11-21 12:04:49 -0800

    Partially added documentation to the createDeck usecase. Still working on it.

--- src/main/java/createDeck/CreateDeckInputData.java
+++ src/main/java/createDeck/CreateDeckInputData.java
@@ -1,14 +1,27 @@
-// a
+/**
+ * This file contains implementation of the CreateDeckInputData class.
+ * It includes an instance attribute of CreateDeckInputData and
+ * the getter methods for it.
+ */
 package createDeck;
 
 public class CreateDeckInputData {
 
     private final String name;
 
+    /**
+     * Constructor for CreateDeckInputData using the given deck name.
+     *
+     * @param name the name of the deck
+     */
     public CreateDeckInputData(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the name of the CreateDeckInputData
+     * @return name of the CreateDeckInputData
+     */
     public String getName() {
         return name;
     }
--- src/main/java/createDeck/CreateDeckInteractor.java
+++ src/main/java/createDeck/CreateDeckInteractor.java
@@ -6,13 +6,26 @@ import createDeck.CreateDeckOutputBoundary;
 import createDeck.CreateDeckOutputData;
 import entities.Deck;
 
+/**
+ * This file contains the implementation of the CreateDeckInteractor class,
+ * which implements CreateDeckInputBoundary.
+ */
 public class CreateDeckInteractor implements CreateDeckInputBoundary {
     final CreateDeckOutputBoundary outputBoundary;
 
+    /**
+     * Constructor for the CreateDeckInteractor
+     *
+     * @param outputBoundary the output associated with this created deck
+     */
     public CreateDeckInteractor(CreateDeckOutputBoundary outputBoundary) {
         this.outputBoundary = outputBoundary;
     }
 
+    /**
+     *
+     * @param inputData
+     */
     @Override
     public void create(CreateDeckInputData inputData) {
         if (Deck.getTracker().containsKey(inputData.getName())){
