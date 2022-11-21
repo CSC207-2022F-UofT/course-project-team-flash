5922c3e807159fabab7074b150b9716d7bad3277
AlbertYS <60264126+AlbertYS@users.noreply.github.com>
2022-11-21 10:15:27 -0500

    Implementation of Jframes and Jpanel classes using swing, prepped for integration with use case controllers

--- src/main/java/screens/DeckScreen.java
+++ src/main/java/screens/DeckScreen.java
@@ -1,7 +1,5 @@
 package screens;
 
-import com.sun.tools.javac.Main;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,6 +8,7 @@ import java.awt.event.ActionListener;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class DeckScreen extends JPanel {
+
     private String textInput;
     private static final int TEXT_FIELD_LENGTH = 10;
 
@@ -31,7 +30,6 @@ public class DeckScreen extends JPanel {
                 }
             }
         });
-
         deckCreationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -41,6 +39,28 @@ public class DeckScreen extends JPanel {
     }
 
     private void chooseDeckCreation() {
+        JFrame newDeckFrame = new JFrame();
+        JDialog newDeckDialog = new JDialog(newDeckFrame);
 
+        JTabbedPane optionTabs = new JTabbedPane();
+
+        JPanel createNewDeck = new JPanel();
+        JPanel importNewDeck = new JPanel();
+
+        optionTabs.add("Create New Deck", createNewDeck);
+        optionTabs.add("Import Deck", importNewDeck);
+
+        newDeckDialog.add(optionTabs);
+
+        JLabel prompt = new JLabel("Enter a name for the new deck:");
+        JTextField createDeckTextField = new JTextField("", TEXT_FIELD_LENGTH);
+        JButton createDeckButton = new JButton("Create Deck");
+        createNewDeck.add(prompt);
+        createNewDeck.add(createDeckTextField);
+        createNewDeck.add(createDeckButton);
+
+        newDeckDialog.pack();
+        newDeckDialog.setModal(true);
+        newDeckDialog.setVisible(true);
     }
 }
