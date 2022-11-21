ea7d53c79d040eb4b7f3761f659d56508482831a
ANQ-Nguyen <93494123+ANQ-Nguyen@users.noreply.github.com>
2022-11-21 11:11:56 -0500

    Initial construction of controllers for the screens that need them.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,36 @@
+import createCard.*;
+import createDeck.*;
+import createQuiz.*;
+import entities.CardFactory;
+import entities.FlashcardFactory;
+
 public class Main {
     public static void main(String[] args) {
         // Build the Jframes
 
         // Call what needs to be called
-
+        CreateCardController createCardController = createCardConstructor();
+        CreateDeckController createDeckController = createDeckConstructor();
+        CreateQuizController createQuizController = createQuizConstructor();
         // IDK what else needs to be done
+    }
+
+    private static CreateCardController createCardConstructor() {
+        CreateCardOutputBoundary presenter = new CreateCardPresenter();
+        CardFactory factory = new FlashcardFactory();
+        CreateCardInputBoundary interactor = new CreateCardInteractor(presenter, factory);
+        return new CreateCardController(interactor);
+    }
+
+    private static CreateDeckController createDeckConstructor() {
+        CreateDeckOutputBoundary presenter = new CreateDeckPresenter();
+        CreateDeckInputBoundary interactor = new CreateDeckInteractor(presenter);
+        return new CreateDeckController(interactor);
+    }
+
+    private static CreateQuizController createQuizConstructor() {
+        CreateQuizOutputBoundary presenter = new CreateQuizPresenter();
+        CreateQuizInputBoundary interactor = new CreateQuizInteractor(presenter);
+        return new CreateQuizController(interactor);
     }
 }
