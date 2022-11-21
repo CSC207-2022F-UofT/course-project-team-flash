30628ee4301a60780eb6becc68ebff1dd3c12af6
tsoisze2 <jasmine.tsoimud@gmail.com>
2022-11-21 18:21:04 -0500

    Added documentations to RunQuiz use case.

--- src/main/java/runQuiz/RunQuizInputBoundary.java
+++ src/main/java/runQuiz/RunQuizInputBoundary.java
@@ -1,3 +1,12 @@
+/**
+ * Author: Jamie Chew
+ * Documentation Author: Jasmine Tsoi
+ * Date: November 21/22
+ *
+ * This file contains an interface named RunQuizInputBoundary with abstract methods startQuiz,
+ * showProblem, and showAnswer.
+ */
+
 package runQuiz;
 
 public interface RunQuizInputBoundary {
--- src/main/java/runQuiz/StartQuizOutputData.java
+++ src/main/java/runQuiz/StartQuizOutputData.java
@@ -1,3 +1,13 @@
+/**
+ * Author: Jamie Chew
+ * Documentation Author: Jasmine Tsoi
+ * Date: November 21/22
+ *
+ * This file contains the implementation of the StartQuizOutputData class.
+ * It includes the instance attributes of the ShowProblemOutputData class and
+ * their getter methods. It also includes the setter method for viewState.
+ */
+
 package runQuiz;
 
 import screens.viewStates;
@@ -10,22 +20,40 @@ public class StartQuizOutputData {
     private int currCardIndex;
     private viewStates viewState;
 
-
-
+    /**
+     * Creates a new StartQuizOutputData given a list of flashcard IDs.
+     *
+     * @param flashcardIds a list of flashcard IDs
+     */
     StartQuizOutputData(String[] flashcardIds){
         this.flashcardIds = flashcardIds;
         this.currCardIndex = 0;
         this.viewState = null;
     }
 
+    /**
+     * Returns the flashcardIds of this StartQuizOutputData.
+     *
+     * @return the flashcardIds of this StartQuizOutputData
+     */
     public String[] getFlashcardIdList(){
         return this.flashcardIds;
     }
 
+    /**
+     * Returns the viewState of this StartQuizOutputData.
+     *
+     * @return the viewState of this StartQuizOutputData
+     */
     public viewStates getViewState(){
         return this.viewState;
     }
 
+    /**
+     * Set the viewState of this StartQuizOutputData.
+     *
+     * @param state the viewStates object to be set as viewState of this StartQuizOutputData
+     */
     public void setViewState(viewStates state){
         this.viewState = state;
     }
