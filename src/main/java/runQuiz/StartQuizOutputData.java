2bcca36bc6c68c0afab69b5ff17d3fc1d2414777
JamieJChew <jamie.chew@mail.utoronto.ca>
2022-11-24 14:54:58 -0500

    Filled in the documentation that was missing due to the new design structure.

--- src/main/java/runQuiz/StartQuizOutputData.java
+++ src/main/java/runQuiz/StartQuizOutputData.java
@@ -8,17 +8,31 @@ public class StartQuizOutputData {
     private ViewState viewState;
 
 
-
+    /**
+     * Creates a new StartQuizOutputData object given a list of flashcard IDs.
+     *
+     * @param flashcardIds
+     */
     StartQuizOutputData(String[] flashcardIds){
         this.flashcardIds = flashcardIds;
         this.currCardIndex = 0;
         this.viewState = null;
     }
 
+    /**
+     * Returns the flashcard IDs as a String array.
+     *
+     * @return the flashcard IDs.
+     */
     public String[] getFlashcardIdList(){
         return this.flashcardIds;
     }
 
+    /**
+     * Returns the current flashcard list index.
+     *
+     * @return the current flascard index.
+     */
     public int getCurrCardIndex(){
         return this.currCardIndex;
     }
