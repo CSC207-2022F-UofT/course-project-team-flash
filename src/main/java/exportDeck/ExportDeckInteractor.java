a14209ca7ff53b2ecead5588cdff14a97c28c3ca
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-24 20:40:07 -0500

    implementation complete, only testing left

--- src/main/java/exportDeck/ExportDeckInteractor.java
+++ src/main/java/exportDeck/ExportDeckInteractor.java
@@ -6,16 +6,40 @@
 
 package exportDeck;
 
-import entities.Deck;
+import entities.*;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class ExportDeckInteractor implements ExportDeckInputBoundary{
     private final ExportDeckOutputBoundary exportDeckOutputBoundary;
 
+    /**
+     * The interactor for the deck export use case
+     * @param exportDeckOutputBoundary output boundary
+     */
     public ExportDeckInteractor(ExportDeckOutputBoundary exportDeckOutputBoundary) {
         this.exportDeckOutputBoundary = exportDeckOutputBoundary;
+    }
+
+    /**
+     * Gets the type of flashcard, returns -1 if invalid
+     * @param card the card to be checked
+     * @return numeric identifier for the card
+     */
+    private String getCardType(Flashcard card){
+        if (card instanceof QandAFlashcard){
+            return "1";
+        }
+        if (card instanceof MCFlashcard){
+            return "2";
+        }
+        if (card instanceof SelectAllFlashcard){
+            return "3";
+        }
+        return "-1";
     }
 
     /**
@@ -27,10 +51,23 @@ public class ExportDeckInteractor implements ExportDeckInputBoundary{
         try {
             String filePath = inputData.getFilePath();
             Deck deckToExport = inputData.getDeckToExport();
+            List<Flashcard> deckCards = deckToExport.getCards();
             String deckFileName = filePath + deckToExport.getName() + ".deck";
             File deckFile = new File(deckFileName);
             FileWriter writer = new FileWriter(deckFileName, false);
-            //writer writes card information to the file...
+            for (Flashcard card : deckCards){
+                String cardType = getCardType(card);
+                if (cardType.equals("-1")){continue;}
+                String cardInfo = cardType + ";" + card.getQuestion() + ";" + card.getAnswer();
+                if (cardType.equals("2")){
+                    String optionInfo = ";";
+                    for (String option : ((MCFlashcard)card).getOptions()){
+                        optionInfo = optionInfo.concat(option+",");
+                    }
+                    cardInfo = cardInfo.concat(optionInfo);
+                }
+                writer.write(cardInfo + System.getProperty("line.separator"));
+            }
             writer.close();
             boolean result = deckFile.createNewFile();
             String message;
