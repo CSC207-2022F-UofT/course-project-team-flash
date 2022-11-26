4cee69dc82b5d5e3aa78b431566acc1b346223c5
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-26 15:12:03 -0500

    created input/output data for gateway

--- src/main/java/importDeck/ImportDeckDsGateway.java
+++ src/main/java/importDeck/ImportDeckDsGateway.java
@@ -1,4 +1,15 @@
+/*
+ * Author: Aldo Bani
+ * Date: November 26/22
+ * This is the gateway for the import system
+ */
+
 package importDeck;
 
 public interface ImportDeckDsGateway {
+    /**
+     * Reads from a file containing information about a deck
+     * @param dsInputData the input data for the gateway
+     */
+    ImportDeckDsOutputData importFromFile(ImportDeckDsInputData dsInputData);
 }
--- src/main/java/importDeck/ImportDeckDsInputData.java
+++ src/main/java/importDeck/ImportDeckDsInputData.java
@@ -1,4 +1,26 @@
+/*
+ * Author: Aldo Bani
+ * Date: November 26/22
+ * This is the input data for the gateway
+ */
+
 package importDeck;
 
 public class ImportDeckDsInputData {
+    private final String filePath;
+
+    /**
+     * Constructor for the gateway input data
+     * @param filePath where the file is located
+     */
+    public ImportDeckDsInputData(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * @return the location of the file
+     */
+    public String getFilePath(){
+        return filePath;
+    }
 }
