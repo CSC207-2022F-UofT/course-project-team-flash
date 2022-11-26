234f5afc7eaf9b8bc282a3c6d2ffcd6dbcdcfab4
tsoisze2 <jasmine.tsoimud@gmail.com>
2022-11-25 21:47:12 -0500

    Corrected documentation in CreateQuiz use case. Also changed implementations of controllers and interactor methods to return void. Implemented CreateQuizPresenter too.

--- src/main/java/createQuiz/CreateQuizPresenter.java
+++ src/main/java/createQuiz/CreateQuizPresenter.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Author: Jasmine Tsoi
  * Documentation Author: Jasmine Tsoi
  * Date: November 21/22
@@ -9,23 +9,41 @@
 
 package createQuiz;
 
+import screens.ViewBoundary;
+import screens.ViewModel;
+import screens.ViewState;
+
 public class CreateQuizPresenter implements CreateQuizOutputBoundary{
+
+    ViewBoundary viewBound;
+
+    public CreateQuizPresenter(ViewBoundary viewBound) {
+        this.viewBound = viewBound;
+    }
 
     /**
      * Prepare the success view given outputData, if the quiz is created and stored successfully.
      *
      * @param outputData the CreateQuizOutputData associated with this created quiz
      */
+    @Override
     public void prepareSuccessView(CreateQuizOutputData outputData){
-
+        ViewModel viewModel = new ViewModel.ViewModelBuilder(ViewState.QUIZ_CREATED).
+                setQuizName(outputData.getQuizName()).build();
+        viewBound.updateView(viewModel);
     }
-
     /**
      * Prepare the fail view given outputData, if the quiz cannot be created or stored successfully.
      *
      * @param outputData the CreateQuizOutputData associated with this created quiz
      */
+    @Override
     public void prepareFailView(CreateQuizOutputData outputData){
+
+        ViewModel viewModel = new ViewModel.ViewModelBuilder(ViewState.ERROR).
+                setReturnString(outputData.getMessage()).build();
+
+        viewBound.updateView(viewModel);
     }
 
 }
