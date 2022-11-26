de006d7f207a90f60127537d030e0170b55ba4c9
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-26 14:20:10 -0500

    created test class

---
+++ src/test/java/importDeck/ImportDeckInteractorTest.java
@@ -0,0 +1,10 @@
+/*
+ * Author: Aldo Bani
+ * Date November 26/22
+ * This is the interactor test for the import deck use case
+ */
+
+package importDeck;
+public class ImportDeckInteractorTest {
+
+}
