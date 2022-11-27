3d051bb3f276c050e2c4a6def9b0b674d16c4dce
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-27 12:03:19 -0500

    finished interactor test and DeckInMemoryImport.java

--- src/main/java/data/DeckInMemoryImport.java
+++ src/main/java/data/DeckInMemoryImport.java
@@ -8,7 +8,36 @@
 package data;
 
 import importDeck.ImportDeckDsGateway;
+import importDeck.ImportDeckDsInputData;
+import importDeck.ImportDeckDsOutputData;
+import screens.ImportDeckFail;
+
+import java.util.List;
+import java.util.Map;
 
 public class DeckInMemoryImport implements ImportDeckDsGateway {
+    private final Map<String, List<String>> file;
 
+    /**
+     * Constructor for deck import from memory
+     * @param file the file for testing
+     */
+    public DeckInMemoryImport(Map<String, List<String>> file){
+        this.file = file;
+    }
+
+    /**
+     * Reads a file and packages the information for use in the interactor
+     * @param dsInputData the input data for the gateway
+     * @return an output data object for use in the interactor
+     */
+    @Override
+    public ImportDeckDsOutputData importFromFile(ImportDeckDsInputData dsInputData){
+        String deckName = dsInputData.getFilePath();
+        List<String> importedCards = file.get(deckName);
+        if (importedCards == null){
+            throw new ImportDeckFail("File doesn't exist");
+        }
+        return new ImportDeckDsOutputData(deckName, importedCards);
+    }
 }
