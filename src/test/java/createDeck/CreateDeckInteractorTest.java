49907a988be787247bf2664ff95f5b3820b61a73
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 10:59:09 -0500

    started work on new deck creation test

--- src/test/java/createDeck/CreateDeckInteractorTest.java
+++ src/test/java/createDeck/CreateDeckInteractorTest.java
@@ -17,13 +17,33 @@ class CreateDeckInteractorTest {
             public void prepareSuccessView(CreateDeckOutputData outputData) {
                 Assertions.assertEquals("Test", Deck.getTracker().get("Test").getName());
             }
+
             @Override
             public void prepareFailView(CreateDeckOutputData outputData) {
-                Assertions.fail(outputData.getText());
+                Assertions.fail("Unexpected failure.");
             }
         };
         CreateDeckInputBoundary interactor = new CreateDeckInteractor(presenter);
         CreateDeckInputData inputData = new CreateDeckInputData("Test");
         interactor.create(inputData);
     }
+    /*
+    @Test
+    public void createDupeDeck(){
+        Deck deckToDelete = new Deck("Test");
+        Deck.addTracker(deckToDelete.getName(), deckToDelete);
+        CreateDeckOutputBoundary presenter = new CreateDeckPresenter(null){
+            @Override
+            public void prepareSuccessView(CreateDeckOutputData outputData) {
+
+            }
+            @Override
+            public void prepareFailView(CreateDeckOutputData outputData) {
+
+            }
+        };
+        CreateDeckInputBoundary interactor = new CreateDeckInteractor(presenter);
+        CreateDeckInputData inputData = new CreateDeckInputData("Test");
+        interactor.create(inputData);
+    }*/
 }
