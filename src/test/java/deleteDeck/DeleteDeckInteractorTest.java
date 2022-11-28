47cdbb08948957e6c118c11df7d09877be9fe498
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-27 20:51:01 -0500

    test for delete deck created

--- src/test/java/deleteDeck/DeleteDeckInteractorTest.java
+++ src/test/java/deleteDeck/DeleteDeckInteractorTest.java
@@ -1,4 +1,35 @@
+/*
+ * Author: Aldo Bani
+ * Date: November 27/22
+ */
+
 package deleteDeck;
 
+import entities.Deck;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class DeleteDeckInteractorTest {
+    @BeforeEach
+    void createDeck(){
+        Deck deckToDelete = new Deck("Test");
+        Deck.addTracker(deckToDelete.getName(), deckToDelete);
+    }
+    @Test
+    void deleteDeck(){
+        DeleteDeckOutputBoundary presenter = new DeleteDeckPresenter(null){
+            @Override
+            public void prepareSuccessView(DeleteDeckOutputData outputData) {
+                Assertions.assertNull(Deck.getTracker().get("Test"));
+            }
+            @Override
+            public void prepareFailView(String exception) {
+                Assertions.fail(exception);
+            }
+        };
+        DeleteDeckInputBoundary interactor = new DeleteDeckInteractor(presenter);
+        DeleteDeckInputData inputData = new DeleteDeckInputData("Test");
+        interactor.delete(inputData);
+    }
 }
