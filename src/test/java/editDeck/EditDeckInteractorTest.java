655426cb4fa2f700a0ef59870accd40bbcf23250
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 10:32:19 -0500

    started working on edit deck test

--- src/test/java/editDeck/EditDeckInteractorTest.java
+++ src/test/java/editDeck/EditDeckInteractorTest.java
@@ -1,4 +1,18 @@
 package editDeck;
 
+import entities.Deck;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class EditDeckInteractorTest {
+    @BeforeEach
+    void createDeck(){
+        Deck deckToDelete = new Deck("Test");
+        Deck.addTracker(deckToDelete.getName(), deckToDelete);
+    }
+    @Test
+    void editDeck(){
+
+    }
 }
