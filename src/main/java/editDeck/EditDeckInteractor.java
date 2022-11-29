36acf2ffbccf2299f8212ec592d7367330798534
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-11-28 19:47:06 -0500

    documentation for the edit deck use case

--- src/main/java/editDeck/EditDeckInteractor.java
+++ src/main/java/editDeck/EditDeckInteractor.java
@@ -1,22 +1,30 @@
-package editDeck;
 /*
  * Author: Jay Kanchankoti
  * Date: November 27/22
  *
  */
 
+package editDeck;
+
 import entities.Deck;
 
 import java.util.Map;
 
 public class EditDeckInteractor  implements EditDeckInputBoundary {
-
     final EditDeckOutputBoundary editDeckOutputBoundary;
 
+    /**
+     * Creates an interactor for the edit deck use case.
+     * @param editDeckOutputBoundary the output boundary for the use case
+     */
     public EditDeckInteractor(EditDeckOutputBoundary editDeckOutputBoundary) {
         this.editDeckOutputBoundary = editDeckOutputBoundary;
     }
 
+    /**
+     * Determines if a deck can be edited. If it can, the name is updated.
+     * @param inputData data from the controller
+     */
     @Override
     public void edit(EditDeckInputData inputData) {
         Map<String, Deck> tracker = Deck.getTracker();
