64b46009572c7982d4c8c85515d638b96da9047b
JamieJChew <jamie.chew@mail.utoronto.ca>
2022-12-02 09:23:38 -0500

    Attempt #2 at fixing the weird github testing bug.

--- src/test/java/entities/QandAFlashcardTest.java
+++ src/test/java/entities/QandAFlashcardTest.java
@@ -1,9 +1,10 @@
 package entities;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.After;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -68,7 +69,13 @@ class QandAFlashcardTest {
         Flashcard card2 = new QandAFlashcard(question, answer);
         Flashcard.addTracker(card2.getUniqueID(), card2);
 
-        Assertions.assertEquals(Flashcard.getTracker().size(), 2);
+        assert Flashcard.getTracker().size() == 2;
     }
-
+    @After
+    public void after() throws Exception {
+        Map<String, Flashcard> tracker = Flashcard.getTracker();
+        for (String key: tracker.keySet()) {
+            tracker.remove(key);
+        }
+    }
 }
