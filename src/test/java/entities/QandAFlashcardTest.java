8d095941c3bb5f3a17f1255d402d70ce75a29cba
JamieJChew <jamie.chew@mail.utoronto.ca>
2022-12-02 10:16:15 -0500

    troubleshooting #12

--- src/test/java/entities/QandAFlashcardTest.java
+++ src/test/java/entities/QandAFlashcardTest.java
@@ -80,7 +80,7 @@ class QandAFlashcardTest {
         Flashcard card2 = new QandAFlashcard(question, answer);
         Flashcard.addTracker(card2.getUniqueID(), card2);
 
-        assert Flashcard.getTracker().size() < 8;
+        assert Flashcard.getTracker().size() < 6;
     }
 
 
