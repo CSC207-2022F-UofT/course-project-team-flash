7c2dae1deb579dd48275ccb7d397b07768fe2fbf
Aldo Bani <aldo.bani@mail.utoronto.ca>
2022-12-04 21:32:10 -0500

    deletequiz and enterdeck tests

--- src/main/java/enterDeck/EnterDeckPresenter.java
+++ src/main/java/enterDeck/EnterDeckPresenter.java
@@ -37,4 +37,11 @@ public class EnterDeckPresenter implements EnterDeckOutputBoundary {
 
         viewBound.updateView(viewModel);
     }
+    @Override
+    public void prepareFailView(String exception){
+        ViewModel viewModel = new ViewModel.ViewModelBuilder(ViewState.ERROR)
+                .setReturnString(exception)
+                .build();
+        viewBound.updateView(viewModel);
+    }
 }
